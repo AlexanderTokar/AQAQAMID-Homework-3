ce237c9fb72c618d8f9c3831508e13d59947b555
AlexanderTokar <alextokar@inbox.ru>
2022-09-06 21:03:36 +0300

    Made mistake at CardOrderTest.java to fail test

---
+++ src/test/java/ru/netology/cardorder/CardOrderTest.java
@@ -0,0 +1,44 @@
+package ru.netology.cardorder;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CardOrderTest {
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupALL() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void shouldSubmitRequestHappyPath() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.name("name")).sendKeys("Джон Голт");
+        driver.findElement(By.name("phone")).sendKeys("+79109101111");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
+    }
+}
